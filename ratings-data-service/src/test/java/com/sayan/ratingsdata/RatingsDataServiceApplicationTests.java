64fc5861a9b7acae790cc82d5fa0a3cb804639ff
msayanece <msayanece@gmail.com>
2020-12-03 13:23:24 +0530

    init commit
    create all 3 services

---
+++ ratings-data-service/src/test/java/com/sayan/ratingsdata/RatingsDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sayan.ratingsdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingsDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
