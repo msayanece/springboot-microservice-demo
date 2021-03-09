73a70024134fcbe8faa87fa42c7080b8fcc7316e
sayanmukherjee <sayan.mukherjee@optimizeitsystems.com>
2021-03-09 12:22:43 +0530

    add hystrix properties in hystrix command

--- movie-catalog-service/src/main/java/com/sayan/moviecatalog/services/RatingService.java
+++ movie-catalog-service/src/main/java/com/sayan/moviecatalog/services/RatingService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.sayan.moviecatalog.models.Rating;
 import com.sayan.moviecatalog.models.RatingResponse;
 
@@ -16,7 +17,19 @@ public class RatingService {
 	@Autowired
 	RestTemplate restTemplate;
 	
-	@HystrixCommand(fallbackMethod = "getRatingsResponseFallback")
+	@HystrixCommand(fallbackMethod = "getRatingsResponseFallback",
+				commandProperties = {
+						@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000"),
+						@HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "5"),
+						@HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "50"),
+						@HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "5000"),
+				},
+				threadPoolKey = "ratingPool",
+				threadPoolProperties = {
+						@HystrixProperty(name = "coreSize", value = "20"),
+						@HystrixProperty(name = "maxQueueSize", value = "10")
+				}
+			)
 	public RatingResponse getRatingsResponse() {
 		RatingResponse ratingResponse = restTemplate.getForObject("http://ratings-data-service/rating/foo", RatingResponse.class);
 		return ratingResponse;
